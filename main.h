2336736c239592798f32047c519513aaf738c404
Tasneem205 <tasneemabdeltawab205@gmail.com>
2025-01-15 04:26:42 +0200

    files and errors files.c

---
+++ main.h
@@ -0,0 +1,62 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdarg.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+extern stack_t *head;
+typedef void (*op_func)(stack_t **, unsigned int);
+
+/* operations of files */
+void open_file(char *fn);
+int parse_line(char *buff, int line_num, int format);
+void read_file(FILE *);
+/* int len_chars(FILE *); */
+void find_func(char *, char *, int, int);
+
+/* stack operations */
+
+/* math operations */
+/* string operatoins */
+/*Error hanlding*/
+void err(int error_code, ...);
+void more_err(int error_code, ...);
+void string_err(int error_code, ...);
+void rotr(stack_t **, unsigned int);
+
+#endif
